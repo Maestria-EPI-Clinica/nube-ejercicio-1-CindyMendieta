82c357212449d3f96ba4734870b109b830dd41f9
CindyMendieta <97898692+CindyMendieta@users.noreply.github.com>
2025-09-13 17:35:22 -0500

    5Update nube.py

--- nube.py
+++ nube.py
@@ -10,7 +10,7 @@ def cargar_datos(ruta_archivo):
     return datos
     
 
-def generar_nube_palabras(ruta_archivo, column_texto, ruta_salida):
+def generar_nube_palabras(datos, column_texto, ruta_salida):
     """Genera una nube de palabras a partir del texto proporcionado."""
 
     ## INICIO - COMPLETAR CODIGO
